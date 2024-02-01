ff7e6d2ad34e4b5a62a98ade7d5fbab3c1fd9003
Shamil <khanmag329@gmail.com>
2024-02-01 20:13:22 +0300

    mw 6

--- src/pages/posts/OnePostPage.jsx
+++ src/pages/posts/OnePostPage.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const OnePostPage = () => {
   const params = useParams()
   // {id: }
+  const [postData, setPostData] = useState(null)
+  useEffect(() => {
+    try {
+      fetch("https://jsonplaceholder.typicode.com/posts/" + params.id)
+        .then(res => {
+          if (res.status === 200) return res.json()
+          throw new Error("fetch error")
+        })
+        .then(data => {
+          console.log(data);
+          setPostData(data)
+        })
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [params.id])
   console.log();
   return (
     <div>
-      <h1>{params.id || ""}</h1>
-      
+      {postData
+        ? (
+          <>
+            <h1>{params.id || ""}</h1>
+            <h2>{postData.title}</h2>
+            <p>{postData.body}</p>
+          </>
+        )
+        : <h2>Не смогли загрузить пост</h2>
+      }
     </div>
   );
 };
--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const PostsPage = () => {
   const [postData, setPostData] = useState([])
   useEffect(() => {
     try {
-      fetch("https://jsonplaceholder.typicode.com/posts/?data=1&page=5")
+      fetch("https://jsonplaceholder.typicode.com/posts")
         .then(res => {
           if (res.status === 200) return res.json()
           throw new Error("fetch error")
@@ -20,8 +21,10 @@ const PostsPage = () => {
   return (
     <div>
       {
-        postData.length && postData.map( item => (
-          <p key={item.id}>{item.title}</p>
+        postData.length && postData.map(item => (
+          <div key={item.id}>
+            <Link to={`/posts/${item.id}`} >{item.title}</Link>
+          </div>
         ))
       }
 
