0e8e930ed43d269e162daeb33bf6a4fd54389df3
Shamil <khanmag329@gmail.com>
2024-02-01 19:46:24 +0300

    mw 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,34 @@ import { Link, Route, Routes } from "react-router-dom"
 import IncomePage from './pages/IncomePage'
 import ExpensesPage from './pages/ExpensesPage'
 import NotFoundPage from './pages/NotFoundPage'
-import Layout from "./Layout"
+// import Layout from "./Layout"
 
 function App() {
 
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<h1>APP</h1>} />
+          <Route path="income" element={<IncomePage />} />
+          <Route path="expenses" element={<ExpensesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes> */}
+      <header>
+        <Link to="/">home</Link>
+        <Link to="/income">income</Link>
+        <Link to="/expenses">expenses</Link>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<h1>APP</h1>} />
           <Route path="/income" element={<IncomePage />} />
           <Route path="/expenses" element={<ExpensesPage />} />
           <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </main>
+      <footer>2024</footer>
     </>
   )
 }
