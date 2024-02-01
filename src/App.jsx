32ce564ba78e62f2e404d7c831a43cbd7f418512
Shamil <khanmag329@gmail.com>
2024-02-01 19:26:13 +0300

    mw 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-
+import { Route, Routes } from "react-router-dom"
+import IncomePage from './pages/IncomePage'
+import ExpensesPage from './pages/ExpensesPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
   return (
     <>
-      <h1>APP</h1>
+      <Routes>
+        <Route path="/" element={<h1>APP</h1>} />
+        <Route path="/income" element={<IncomePage />} />
+        <Route path="/expenses" element={<ExpensesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   )
 }
