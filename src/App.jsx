0ee6e4d1d634f39c4c531a3ea6e41e0b2395fb8f
Shamil <khanmag329@gmail.com>
2024-02-05 19:38:53 +0300

    mw 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { Link, NavLink, Route, Routes } from "react-router-dom"
+import { NavLink, Route, Routes } from "react-router-dom"
 import IncomePage from './pages/IncomePage'
 import ExpensesPage from './pages/ExpensesPage'
 import NotFoundPage from './pages/NotFoundPage'
 import PostsPage from "./pages/posts"
 import OnePostPage from "./pages/posts/OnePostPage"
-// import Layout from "./Layout"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
 
   return (
     <>
-      {/* <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<h1>APP</h1>} />
-          <Route path="income" element={<IncomePage />} />
-          <Route path="expenses" element={<ExpensesPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes> */}
       <header>
         <NavLink className={({isActive}) => isActive ? "black_border" : ""} to="/">home</NavLink>
         <NavLink to="/income">income</NavLink>
@@ -26,7 +18,7 @@ function App() {
       </header>
       <main>
         <Routes>
-          <Route path="/" element={<h1>APP</h1>} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/income" element={<IncomePage />} />
           <Route path="/expenses" element={<ExpensesPage />} />
           <Route path="/posts" element={<PostsPage />} />
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,22 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Box>
+        <Typography>LOGIN</Typography>
+        <Box>
+          <TextField
+          />
+        </Box>
+        <Box>
+          <TextField
+          />
+        </Box>
+        <Button variant="outlined">Log In</Button>
+      </Box>
+    </div>
+  );
+};
+
+export default LoginPage;
