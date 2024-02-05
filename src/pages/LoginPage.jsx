1aa877910b77b0f4a1b58cdc8ff2661d0b121039
Shamil <khanmag329@gmail.com>
2024-02-05 19:46:04 +0300

    mw 3

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,19 +1,37 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { emailValidator } from "../utils/validators";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("")
+  const [pass, setPass] = useState("")
+
+  const handleLogIn = () => {
+    
+  }
   return (
     <div>
       <Box>
         <Typography>LOGIN</Typography>
         <Box>
           <TextField
+            // type="email"
+            label="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            error={!!email && !emailValidator(email)}
           />
         </Box>
         <Box>
           <TextField
+            // type="password"
+            label="pass"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            error={pass.length < 6}
           />
         </Box>
-        <Button variant="outlined">Log In</Button>
+        <Button variant="outlined" onClick={handleLogIn}>Log In</Button>
       </Box>
     </div>
   );
